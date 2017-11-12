a12d3d92118598377aa400f4d063c76ad38b1c1e
wanjt <zzuwanjiangtao@163.com>
2017-11-12 20:29:41 +0800

    增加包main，蓝本main，定义main中的视图

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from config import config
+from .main import main as main_blueprint
 
 db = SQLAlchemy()
 
@@ -9,4 +10,5 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object(config)
     db.init_app(app)
+    app.register_blueprint(main_blueprint)
     return app
